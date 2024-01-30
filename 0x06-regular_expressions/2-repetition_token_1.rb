e85acc0f96f39de0fb4f4dc69241d878e74a0e9d
Tsehafetizaz <wendetefe@gmail.com>
2024-01-30 04:39:49 -0800

    test

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if a single argument is provided
-if ARGV.length != 1
-  puts "Usage: #{__FILE__} <string>"
-  exit(1)
-end
-
-# Regular expression pattern to match the test string
-pattern = /hbt*n/
-
-# Get the input string from the command-line argument
-input_string = ARGV[0]
-
-# Match the input string against the pattern
-if input_string.match?(pattern)
-  puts input_string
-end
+puts ARGV[0].scan(/hb?t?n/).join

378086b07207e00118debb142dfd9793b9807397
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 11:22:05 -0700

    degree

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,21 @@
+# Puppet manifest to add a custom HTTP response header to Nginx
+# This manifest configures Nginx on a new Ubuntu machine to include a custom HTTP header
+# in its response, indicating the hostname of the server handling the request.
+#
+# Requirements:
+# - Nginx should be installed
+# - Custom HTTP header 'X-Served-By' should be added to responses
+
 class nginx_custom_header {
   package { 'nginx':
     ensure => installed,
   }
 
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$hostname;
-}",
+  file { '/etc/nginx/nginx.conf':
+    ensure => present,
+    content => "user www-data;\nworker_processes auto;\npid /run/nginx.pid;\n\nevents {\n\tworker_connections 768;\n\t# multi_accept on;\n}\n\nhttp {\n\t...\n\tserver {\n\t\t...\n\t\tlocation / {\n\t\t\tadd_header X-Served-By $hostname;\n\t\t\t...\n\t\t}\n\t\t...\n\t}\n}",
     require => Package['nginx'],
-    notify  => Service['nginx'],
+    notify => Service['nginx'],
   }
 
   service { 'nginx':
@@ -20,5 +23,3 @@ server {
     enable => true,
   }
 }
-
-include nginx_custom_header

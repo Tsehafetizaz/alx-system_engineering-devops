bf1fc99226af6c5766652aa2e4d7a0e057919daf
Tsehafetizaz <wendetefe@gmail.com>
2024-04-17 12:08:49 -0700

    temap

--- 0x14-mysql/createuser.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# This script creates a replication user on the primary MySQL server
-
-mysql -uroot -proot <<MYSQL_SCRIPT
-CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replica_password';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
--- 0x14-mysql/install.sh
+++ 0x14-mysql/install.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
-# This script installs MySQL version 5.7.x on Ubuntu 16.04
+# Installs MySQL 5.7 and sets up the necessary directories and permissions.
 
-# Update package information, ensuring that APT works with the https method, and that CA certificates are installed
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates
-
-# Add Oracle's repository for MySQL 5.7
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb
-
-# Update package information with the new repository
-sudo apt-get update
-
-# Install MySQL Server in a non-interactive mode. Default root password will be "root"
-echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
-sudo apt-get -y install mysql-server-5.7
-
-# Check MySQL version
-mysql --version
+sudo apt-get install mysql-server-5.7

9c95552c2701bc18e6c4c3c2075e7b59dad40fc3
Tsehafetizaz <wendetefe@gmail.com>
2024-04-22 06:36:37 -0700

    ene

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-import requests
+"""Exports to-do list information of all employees to JSON format."""
 import json
+import requests
 
 
-def fetch_all_tasks():
-    """Fetch and export tasks of all employees to a JSON file."""
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+def fetch_and_export_todos():
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users_url = f"{base_url}users"
+    todos_url = f"{base_url}todos"
 
+    # Fetch users
     users = requests.get(users_url).json()
-    todos = requests.get(todos_url).json()
 
-    user_tasks = {}
+    all_todos = {}
+    # Fetch todos for each user
     for user in users:
-        filtered_tasks = [{
-            "username": user['username'],
-            "task": todo['title'],
-            "completed": todo['completed']
-        } for todo in todos if todo['userId'] == user['id']]
-        user_tasks[str(user['id'])] = filtered_tasks
+        user_id = user.get("id")
+        todos = requests.get(todos_url, params={"userId": user_id}).json()
+        all_todos[user_id] = [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user.get("username")
+        } for todo in todos]
 
-    with open("todo_all_employees.json", mode='w') as file:
-        json.dump(user_tasks, file)
+    # Write to JSON file
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_todos, jsonfile)
 
 
 if __name__ == "__main__":
-    fetch_all_tasks()
+    fetch_and_export_todos()

e9ea17d51e23e61da920c55e0a0881e3d74aa70b
Tsehafetizaz <wendetefe@gmail.com>
2024-04-22 06:30:10 -0700

    ene

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+"""Script that uses a REST API to return information about an employee's
+TODO list progress for a given employee ID."""
+
+import requests
 import sys
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+
+    user = requests.get(f'{base_url}users/{user_id}').json()
+    todos = requests.get(f'{base_url}todos', params={'userId': user_id}).json()
+
+    completed = [task["title"] for task in todos if task.get('completed')]
+
+    # Break the line to comply with PEP8 line length constraints
+    message = (f"Employee {user.get('name')} is done "
+               f"with tasks({len(completed)}/{len(todos)}):")
+    print(message)
+    for title in completed:
+        print(f"\t {title}")

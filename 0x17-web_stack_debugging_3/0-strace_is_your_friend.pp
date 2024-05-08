ad2bef997bc1c5592bcbcdf35c040dd7ec73092d
Tsehafetizaz <wendetefe@gmail.com>
2024-05-08 06:18:59 -0700

    tezsa

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,11 @@
 # 0-strace_is_your_friend.pp
-# This Puppet manifest fixes a common Apache 500 error by ensuring proper permissions
-# and presence of a necessary configuration file.
 
-# Ensure the Apache configuration file exists and has correct permissions
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['apache2'],
-}
+# This comment describes the purpose of the Puppet manifest
+# This manifest fixes permission errors on a specific file
 
-# Restart Apache to apply changes and fix the error
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/apache2/apache2.conf'],
-  require    => File['/etc/apache2/apache2.conf'],
+file { '/var/www/html/error.log':
+  ensure => present,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0644',
 }

01fc4606fbcc86b84b16fff48007f5922e469778
Tsehafetizaz <wendetefe@gmail.com>
2024-05-09 06:20:34 -0700

    tera

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""
-Module to fetch and display the titles of the top ten hot posts of a given
-subreddit.
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) '
-                      'Gecko/20100101 Firefox/52.0'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
+    params = {
+        "limit": 10
+    }
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        if response.status_code != 200:
+            print("None")
+            return
+        results = response.json().get("data", {}).get("children", [])
+        for child in results:
+            title = child.get("data", {}).get("title")
+            if title:  # Ensure that title is not None
+                print(title)
     except requests.RequestException:
-        print(None)
+        print("None")
 
 
 if __name__ == "__main__":

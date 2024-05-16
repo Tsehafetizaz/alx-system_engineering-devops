a2a4ccc3d03229bde6eff1aa6443fdc98cf85caa
Tsehafetizaz <wendetefe@gmail.com>
2024-05-16 00:27:39 -0700

    surprise

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,21 +1,23 @@
-# This Puppet manifest increases the file descriptor limit for the holberton user to prevent errors due to too many open files.
+# This Puppet manifest increases the file limits for the holberton user to prevent "Too many open files" errors.
 
-exec { 'increase-file-descriptor-limit':
-  command => 'echo "* soft nofile 65536\n* hard nofile 65536\nholberton soft nofile 65536\nholberton hard nofile 65536" >> /etc/security/limits.conf',
-  path    => ['/bin', '/usr/bin'],
-  unless  => 'grep "holberton hard nofile 65536" /etc/security/limits.conf',
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard nofile/s/[0-9]*/50000/" /etc/security/limits.conf || echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  unless  => 'grep -q "holberton hard nofile 50000" /etc/security/limits.conf',
 }
 
-exec { 'increase-pam-limits':
+# Increase soft file limit for holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft nofile/s/[0-9]*/50000/" /etc/security/limits.conf || echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  unless  => 'grep -q "holberton soft nofile 50000" /etc/security/limits.conf',
+}
+
+# Ensure PAM module is configured to apply limits
+exec { 'ensure-pam-limits':
   command => 'echo "session required pam_limits.so" >> /etc/pam.d/common-session',
-  path    => ['/bin', '/usr/bin'],
-  unless  => 'grep "session required pam_limits.so" /etc/pam.d/common-session',
-  require => Exec['increase-file-descriptor-limit'],
-}
-
-exec { 'reload-ssh':
-  command     => 'service ssh reload',
-  path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  refreshonly => true,
-  subscribe   => Exec['increase-pam-limits'],
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  unless  => 'grep -q "session required pam_limits.so" /etc/pam.d/common-session',
+  require => [Exec['increase-hard-file-limit-for-holberton-user'], Exec['increase-soft-file-limit-for-holberton-user']],
 }

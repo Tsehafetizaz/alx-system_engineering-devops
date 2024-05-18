3bd547ef2cd070a582091e72440f3f07b1865444
Tsehafetizaz <wendetefe@gmail.com>
2024-05-18 15:31:00 -0700

    corporate

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,6 @@
-# This Puppet manifest increases the ULIMIT for Nginx and restarts the Nginx server.
-
-# Ensure the ULIMIT value is correctly set and uncommented in /etc/default/nginx
-exec { 'fix-ulimit-for-nginx':
-  command => 'sed -i "s/#ULIMIT=.*/ULIMIT=\\"-n 4096\\"/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
-  unless  => 'grep -q "^ULIMIT=\\"-n 4096\\"" /etc/default/nginx',
-}
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => ['/usr/local/bin', '/bin', '/usr/sbin', '/etc/init.d'],
-  require => Exec['fix-ulimit-for-nginx'],
+# Fixes an nginx site that can't handle multiple concurrent requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
